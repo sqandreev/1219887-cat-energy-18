81bc766eb01041bd8c5736c10212856de3d9fe39
Vladislav Andreev <sqandreev@yandex.ru>
2019-11-23 06:25:58 +0300

    Написал стили шапки

---
+++ source/js/common.js
@@ -0,0 +1,13 @@
+;(() => {
+  "use strict";
+
+  const mainNavMenu = document.querySelector(".main-nav__menu");
+  const mainNavList = document.querySelector(".main-nav__list");
+
+  const showMenu = () => {
+    mainNavMenu.classList.toggle("main-nav__menu--active");
+    mainNavList.classList.toggle("main-nav__list--active");
+  };
+
+  mainNavMenu.addEventListener("click", showMenu);
+})();
