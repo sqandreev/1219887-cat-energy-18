96b5a39bc1e66e9d5f987c03b4e94f9fbc71c0c9
Vladislav Andreev <sqandreev@yandex.ru>
2019-12-20 23:58:17 +0300

    Доделал блок с фильтром

--- source/js/filter.js
+++ source/js/filter.js
@@ -12,6 +12,11 @@
     filterImageAfter.classList.add("transformation__image--hidden");
     filterRangeButton.setAttribute("aria-label", "Было");
     filterRangeButton.classList.remove("transformation__filter-button--after");
+    filterRangeButton.classList.add("transformation__filter-button--before");
+
+    filterImageBefore.classList.add("transformation__image--visible");
+    filterImageBefore.classList.remove("transformation__image--invisible");
+    filterImageAfter.classList.add("transformation__image--invisible");
   });
 
   filterStateAfter.addEventListener("click", () => {
@@ -19,5 +24,9 @@
     filterImageBefore.classList.add("transformation__image--hidden");
     filterRangeButton.setAttribute("aria-label", "Стало");
     filterRangeButton.classList.add("transformation__filter-button--after");
+
+    filterImageAfter.classList.add("transformation__image--visible");
+    filterImageAfter.classList.remove("transformation__image--invisible");
+    filterImageBefore.classList.add("transformation__image--invisible");
   });
 })();
