81abd13ea1487e7ae1be79e24128b6483712e944
Vladislav Andreev <sqandreev@yandex.ru>
2020-01-23 21:27:26 +0300

    Update files

--- build/js/common.js
+++
@@ -1,1 +0,0 @@
-!function(){"use strict";var e=document.querySelector(".main-nav__menu"),n=document.querySelector(".main-nav__list");e.addEventListener("click",function(){e.classList.toggle("main-nav__menu--active"),n.classList.toggle("main-nav__list--active")})}();
