515fd4624604b66210208cfca118220e79cb4b9e
Vladislav Andreev <sqandreev@yandex.ru>
2020-01-24 22:18:31 +0300

    Исправил код под Б30

--- source/js/common.js
+++ source/js/common.js
@@ -3,6 +3,9 @@
 
   var mainNavMenu = document.querySelector(".main-nav__menu");
   var mainNavList = document.querySelector(".main-nav__list");
+
+  mainNavMenu.classList.remove("main-nav__menu--active");
+  mainNavList.classList.remove("main-nav__list--active");
   
   var showMenu = function() {
     mainNavMenu.classList.toggle("main-nav__menu--active");
